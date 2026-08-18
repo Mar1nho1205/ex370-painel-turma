98b911eeb7e11c863522a1791fc17e8a81def3fd
Mar1nho1205 <silvajota265@gmail.com>
2026-08-18 14:37:38 -0300

    segunda

--- src/rotas/links.js
+++ src/rotas/links.js
@@ -9,13 +9,26 @@ let proximoId = 1;
 
 // GET /links — lista todos os links.
 router.get("/", (req, res) => {
-  // TODO (Tarefa B): responda com status 200 e o array `links`.
-  res.status(501).json({ erro: "não implementado" });
+  res.status(200).json(links);
 });
 
 // POST /links — cria um link { titulo, url } (ambos TEXTO/string).
 router.post("/", (req, res) => {
   // TODO (Tarefa B):
+  const { titulo, url } = req.body;
+
+  if(!titulo || !url) {
+    return res.status(400).json({ erro: "Há elementos faltando "})
+  }
+
+  const novoLink = {
+    id: proximoId++,
+    titulo,
+    url
+  };
+
+  links.push(novoLink);
+  res.status(400).json(novoLink);
   //  1. Leia titulo (texto) e url (texto) de req.body.
   //  2. Se faltar titulo OU url, responda 400.
   //  3. Crie { id: proximoId++, titulo, url }, adicione em `links`
